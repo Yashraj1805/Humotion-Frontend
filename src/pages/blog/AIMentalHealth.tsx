dbfc360a80365375b83d398a15bf8c6795c6c181
Theyashrajdubey <dyashraj1805@gmail.com>
2025-07-24 20:20:48 +0530

    gm

--- src/pages/blog/AIMentalHealth.tsx
+++ src/pages/blog/AIMentalHealth.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from "@/components/Header";
+import Header from "@/components/Header"; 
 import { motion, useScroll, useTransform } from 'framer-motion';
 import { FaCalendarAlt, FaClock, FaShare, FaHeart, FaTwitter, FaLinkedin, FaFacebook, FaWhatsapp } from 'react-icons/fa';
 
