8a45010b247e68ab8bcac8164445c77d10391740
Yash Raj Dubey <94584095+Yashraj1805@users.noreply.github.com>
2025-08-15 15:47:27 +0530

    Update Register.tsx

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -87,7 +87,7 @@ const Register = () => {
           password: formData.password,
           confirmPassword: formData.confirmPassword,
           company: formData.company,
-          phone: formData.phone,
+          phone: formData.phoneNumber,
           location: formData.location
         },
         {
